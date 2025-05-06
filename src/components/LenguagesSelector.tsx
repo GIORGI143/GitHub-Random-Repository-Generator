8a84dd1fe6e403cbb71ab278cfa26e456ae7496f
GIORGI143 <giorgikharshiladze443@gmail.com>
2025-05-06 15:47:16 +0400

    fixed some UI bugs

--- src/components/LenguagesSelector.tsx
+++ src/components/LenguagesSelector.tsx
@@ -32,41 +32,43 @@ const LenguagesSelector = () => {
   }, [modalIsOpen]);
 
   return (
-    <div
-      ref={modalRef}
-      className="drop-down-menu"
-      style={{ overflowY: modalIsOpen ? "scroll" : "hidden" }}
-    >
+    <div className="lenguages-selector">
       <div
-        className="selected-element"
-        onClick={() => setModalIsOpen(!modalIsOpen)}
+        ref={modalRef}
+        className="drop-down-menu"
+        style={{ overflowY: modalIsOpen ? "scroll" : "hidden" }}
       >
-        {selectedLenguage?.title}
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          style={{
-            transform: modalIsOpen ? "rotate(-90deg)" : "rotate(90deg)",
-          }}
+        <div
+          className="selected-element"
+          onClick={() => setModalIsOpen(!modalIsOpen)}
         >
-          <polygon points="7.293 4.707 14.586 12 7.293 19.293 8.707 20.707 17.414 12 8.707 3.293 7.293 4.707" />
-        </svg>
-      </div>
-      {modalIsOpen &&
-        lenguagesData.map((lenguage) => (
-          <div
-            className="element"
-            key={lenguage.value}
-            onClick={() => {
-              setSelectedLenguage(lenguage);
-              setModalIsOpen(false);
+          {selectedLenguage?.title}
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            style={{
+              transform: modalIsOpen ? "rotate(-90deg)" : "rotate(90deg)",
             }}
           >
-            {lenguage.title}
-          </div>
-        ))}
+            <polygon points="7.293 4.707 14.586 12 7.293 19.293 8.707 20.707 17.414 12 8.707 3.293 7.293 4.707" />
+          </svg>
+        </div>
+        {modalIsOpen &&
+          lenguagesData.map((lenguage) => (
+            <div
+              className="element"
+              key={lenguage.value}
+              onClick={() => {
+                setSelectedLenguage(lenguage);
+                setModalIsOpen(false);
+              }}
+            >
+              {lenguage.title}
+            </div>
+          ))}
+      </div>
     </div>
   );
 };
